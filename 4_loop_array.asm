e8b3e7fcb2b0ba482f41651faddfb078a69baedd
luc-dev10 <luc.dev1008@gmail.com>
2022-03-01 21:16:05 -0500

    loop array

--- 4_loop_array.asm
+++ 4_loop_array.asm
@@ -1,14 +1,10 @@
-n: 	DD 	1, 2, 3, 4
-
-	addi t0, x0, 0 	    ;; counter
-	addi t1, x0, 32	    ;; size limit
-	ld t2, n(t0)
-LOOP:		
-	ecall x0, t2, 0
-	addi t0, t0, 8
-	beq t1, t0, EXIT
-	ld t2, n(t0)
-	bne t1, t0, LOOP			    ;; continue loop
-EXIT:
-	ecall x0, t0, 0
-	ebreak x0, x0, 0;
+src: DD -1, 5, -3, 7, 0
+add x6, x0, x0
+loop: ld x5, src(x6)
+beq x5, x0, end
+ecall x0, x5, 0
+sd x5, dst(x6)
+addi x6, x6, 8
+beq x0, x0, loop
+end: ebreak x0, x0, 0
+dst: DM 1
